2bb9813c31a647e89dbec1e5e12e0c092114a671
jonmcwong <jonmcwong@gmail.com>
2020-06-24 03:06:57 +0100

    changed echos in easy datagen

--- easy_datagen.sh
+++ easy_datagen.sh
@@ -5,7 +5,45 @@
 
 if [[ $ZONE == "" ]] ; then
     echo "ZONE variable not defined"
-elif [[ $# -eq 1 ]] ; then
+elif [[ $# -eq 0 ]] ; then
+    if [[ $ZONE == "us-central1-f" ]] ; then
+        export STORAGE_BUCKET=gs://us_bucketbucket
+    elif [[ $ZONE == "europe-west4-a" ]] ; then
+        export STORAGE_BUCKET=gs://mathsreasoning
+    else
+        echo
+        echo
+        echo
+        echo "ZONE variable is weird... ZONE = "$ZONE
+        echo
+        echo
+        echo
+    fi
+
+    export PROBLEM=algorithmic_math_deepmind_all
+    # datagen will recognise the "easy part and select only the train-easy data"
+    export DATA_DIR=${STORAGE_BUCKET}/t2t-easy-data
+    export TMP_DIR=${STORAGE_BUCKET}/t2t-easy-data-incomplete
+    export SPECIFIC_SPLITS=easy
+
+    echo
+    echo "Running..."
+    echo "t2t-datagen \\"
+    echo "  --problem=$PROBLEM \\"
+    echo "  --data_dir=$DATA_DIR \\"
+    echo "  --tmp_dir=$TMP_DIR \\"
+    echo "  --specific_splits=$SPECIFIC_SPLITS"
+    echo
+
+
+    t2t-datagen \
+      --problem=$PROBLEM \
+      --data_dir=$DATA_DIR \
+      --tmp_dir=$TMP_DIR \
+      --specific_splits=$SPECIFIC_SPLITS
+
+
+elif [[ $# -eq 1 && $1 == "--dry-run" ]]; then
     if [[ $ZONE == "us-central1-f" ]] ; then
         export STORAGE_BUCKET=gs://us_bucketbucket
     elif [[ $ZONE == "europe-west4-a" ]] ; then
@@ -34,51 +72,5 @@ elif [[ $# -eq 1 ]] ; then
     echo "  --tmp_dir=$TMP_DIR \\"
     echo "  --specific_splits=$SPECIFIC_SPLITS"
     echo
-
-
-    t2t-datagen \
-      --problem=$PROBLEM \
-      --data_dir=$DATA_DIR \
-      --tmp_dir=$TMP_DIR \
-      --specific_splits=$SPECIFIC_SPLITS
-
-
-elif [[ $# -eq 2 && $2 == "--dry-run" ]]; then
-    if [[ $ZONE == "us-central1-f" ]] ; then
-        export STORAGE_BUCKET=gs://us_bucketbucket
-    elif [[ $ZONE == "europe-west4-a" ]] ; then
-        export STORAGE_BUCKET=gs://mathsreasoning
-    else
-        echo
-        echo
-        echo
-        echo "ZONE variable is weird... ZONE = "$ZONE
-        echo
-        echo
-        echo
-    fi
-
-    export PROBLEM=algorithmic_math_deepmind_all
-    # datagen will recognise the "easy part and select only the train-easy data"
-    export DATA_DIR=${STORAGE_BUCKET}/t2t-easy-data
-    export TMP_DIR=${STORAGE_BUCKET}/t2t-easy-data-incomplete
-    export SPECIFIC_SPLITS=easy
-
-    echo
-    echo "Process will run the following:"
-    echo "t2t-datagen \\"
-    echo "  --problem=$PROBLEM \\"
-    echo "  --data_dir=$DATA_DIR \\"
-    echo "  --tmp_dir=$TMP_DIR \\"
-    echo "  --specific_splits=$SPECIFIC_SPLITS"
-    echo
-
-
-    t2t-datagen \
-      --problem=$PROBLEM \
-      --data_dir=$DATA_DIR \
-      --tmp_dir=$TMP_DIR \
-      --specific_splits=$SPECIFIC_SPLITS
-
 
 fi
