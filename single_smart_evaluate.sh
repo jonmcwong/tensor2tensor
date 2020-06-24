c5bfabc1c87dea622ee186cbb7fd01f2b22a0461
jonmcwong <jonmcwong@gmail.com>
2020-06-24 15:35:04 +0100

    spacing

--- single_smart_evaluate.sh
+++ single_smart_evaluate.sh
@@ -68,52 +68,52 @@ if [[ $# -eq 3 ]] ; then
 
 
 
-        echo
-        echo "Process will run the following:"
-        echo "TPU_IP_ADDRESS = "$TPU_IP_ADDRESS
-        echo "XRT_TPU_CONFIG = "$XRT_TPU_CONFIG
-        echo "t2t-eval \\"
-        echo "    --problem=$PROBLEM \\"
-        echo "    --model=$MODEL \\"
-        echo "    --data_dir=$DATA_DIR \\"
-        echo "    --output_dir=$TRAIN_DIR \\"
-        echo "    --eval_use_test_set=$EVAL_USE_TEST_SET \\"
-        echo "    --hparams_set=$HPARAMS_SET \\"
-        echo "    --dataset_split=$DATASET_SPLIT \\"
-        echo "    --use_tpu=$USE_TPU \\"
-        echo "    --cloud_tpu_name=$CLOUD_TPU_NAME \\"
-        echo "    --eval_steps=3 \\"
-        echo "    --results_dir=$RESULTS_DIR"
-        echo
-        t2t-eval \
-            --problem=$PROBLEM \
-            --model=$MODEL \
-            --data_dir=$DATA_DIR \
-            --output_dir=$TRAIN_DIR \
-            --eval_use_test_set=$EVAL_USE_TEST_SET \
-            --hparams_set=$HPARAMS_SET \
-            --dataset_split=$DATASET_SPLIT \
-            --use_tpu=$USE_TPU \
-            --cloud_tpu_name=$CLOUD_TPU_NAME \
-            --eval_steps=3 \
-            --results_dir=$RESULTS_DIR
-        echo
-        echo "Process will run the following:"
-        echo "TPU_IP_ADDRESS = "$TPU_IP_ADDRESS
-        echo "XRT_TPU_CONFIG = "$XRT_TPU_CONFIG
-        echo "t2t-eval \\"
-        echo "    --problem=$PROBLEM \\"
-        echo "    --model=$MODEL \\"
-        echo "    --data_dir=$DATA_DIR \\"
-        echo "    --output_dir=$TRAIN_DIR \\"
-        echo "    --eval_use_test_set=$EVAL_USE_TEST_SET \\"
-        echo "    --hparams_set=$HPARAMS_SET \\"
-        echo "    --dataset_split=$DATASET_SPLIT \\"
-        echo "    --use_tpu=$USE_TPU \\"
-        echo "    --cloud_tpu_name=$CLOUD_TPU_NAME \\"
-        echo "    --eval_steps=3 \\"
-        echo "    --results_dir=$RESULTS_DIR"
-        echo
+    echo
+    echo "Running..."
+    echo "TPU_IP_ADDRESS = "$TPU_IP_ADDRESS
+    echo "XRT_TPU_CONFIG = "$XRT_TPU_CONFIG
+    echo "t2t-eval \\"
+    echo "    --problem=$PROBLEM \\"
+    echo "    --model=$MODEL \\"
+    echo "    --data_dir=$DATA_DIR \\"
+    echo "    --output_dir=$TRAIN_DIR \\"
+    echo "    --eval_use_test_set=$EVAL_USE_TEST_SET \\"
+    echo "    --hparams_set=$HPARAMS_SET \\"
+    echo "    --dataset_split=$DATASET_SPLIT \\"
+    echo "    --use_tpu=$USE_TPU \\"
+    echo "    --cloud_tpu_name=$CLOUD_TPU_NAME \\"
+    echo "    --eval_steps=3 \\"
+    echo "    --results_dir=$RESULTS_DIR"
+    echo
+    t2t-eval \
+        --problem=$PROBLEM \
+        --model=$MODEL \
+        --data_dir=$DATA_DIR \
+        --output_dir=$TRAIN_DIR \
+        --eval_use_test_set=$EVAL_USE_TEST_SET \
+        --hparams_set=$HPARAMS_SET \
+        --dataset_split=$DATASET_SPLIT \
+        --use_tpu=$USE_TPU \
+        --cloud_tpu_name=$CLOUD_TPU_NAME \
+        --eval_steps=3 \
+        --results_dir=$RESULTS_DIR
+    echo
+    echo "Process ran with these settings:"
+    echo "TPU_IP_ADDRESS = "$TPU_IP_ADDRESS
+    echo "XRT_TPU_CONFIG = "$XRT_TPU_CONFIG
+    echo "t2t-eval \\"
+    echo "    --problem=$PROBLEM \\"
+    echo "    --model=$MODEL \\"
+    echo "    --data_dir=$DATA_DIR \\"
+    echo "    --output_dir=$TRAIN_DIR \\"
+    echo "    --eval_use_test_set=$EVAL_USE_TEST_SET \\"
+    echo "    --hparams_set=$HPARAMS_SET \\"
+    echo "    --dataset_split=$DATASET_SPLIT \\"
+    echo "    --use_tpu=$USE_TPU \\"
+    echo "    --cloud_tpu_name=$CLOUD_TPU_NAME \\"
+    echo "    --eval_steps=3 \\"
+    echo "    --results_dir=$RESULTS_DIR"
+    echo
 
 elif [[ $# -eq 4 && $4 == "--dry-run" ]]; then
     export VM_IP=$(echo $SSH_CONNECTION | sed "s/^.* \([0-9|\.]*\) [0-9]*$/\1/")
