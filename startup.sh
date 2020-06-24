92551c0f9beaebb8b2f89d7cfa3d431fe6e2faff
jonmcwong <jonmcwong@gmail.com>
2020-06-24 16:23:03 +0100

    add trace to eval

--- startup.sh
+++ startup.sh
@@ -10,10 +10,14 @@ chmod +x single_smart_evaluate.sh
 
 git checkout .
 git pull
-chmod +x single_smart_evaluate.sh
 ./setup.sh
+screen
+ 
+./single_smart_evaluate.sh transformer base_test-dropout01-2020-06-24 1
+
 ./single_smart_evaluate.sh transformer quick-noam-dropout01-2020-06-21 2
 
+chmod +x single_smart_evaluate.sh
 
 gsutil -m cp -r \
 gs://us_bucketbucket/results-universal_transformer-ut-lowerlr0-002-2020-06-23 \
@@ -26,9 +30,13 @@ git clone https://github.com/jonmcwong/PyTorch-Beam-Search-Decoding.git
 plt.clf()
 plot_against_steps(make_md([
 
-    "universal_transformer-lowerlr0-02-2020-06-23",
-
-    "universal_transformer-ut-lowerlr0-002-2020-06-23",
+    "transformer-noam-dropout01-2020-06-20",
     ], [
-"all"
-    ]), xlim=(-10000, 200000), title="???????????", save_name="Latest_plot.png", font_size=15)
+    "extra_add_or_sub_big",
+    "extra_mul_big",
+
+    "inter_mul",
+
+    "inter_add_or_sub",
+
+    ]), xlim=(0, 1200000), title="???????????", save_name="Latest_plot.png", font_size=12)
