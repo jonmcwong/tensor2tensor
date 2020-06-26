24557bcec8661e0bf32f37159f520aecad74f6b3
Jonathan Wong <jonmcwong@gmail.com>
2020-06-26 01:32:03 +0100

    update single eval

--- single_smart_evaluate.sh
+++ single_smart_evaluate.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+
+# Get VM and tpu info
 export VM_IP=$(echo $SSH_CONNECTION | sed "s/^.* \([0-9|\.]*\) [0-9]*$/\1/")
 export VM_INFO=$(gcloud compute instances list | grep $VM_IP)
 export VM_NAME=$(echo $VM_INFO | cut -d' ' -f1)
@@ -7,7 +10,13 @@ export VM_ZONE=$(echo $VM_INFO | cut -d' ' -f2)
 export TPU_INFO=$(gcloud compute tpus list --zone=$VM_ZONE | grep $VM_NAME)
 export TPU_IP=$(echo $TPU_INFO | sed "s/^.*v[0-9].*\s\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\):[0-9]*\s.*$/\1/")
 export TPU_NAME=$(echo $TPU_INFO | cut -d' ' -f1)
+# Assumes the VM has a tpu already configured
+export TPU_IP_ADDRESS=$TPU_IP
+export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
 
+
+
+# set bucket based on vm location
 if [[ $VM_ZONE == "us-central1-a" || $VM_ZONE == "us-central1-b" || $VM_ZONE == "us-central1-c" || $VM_ZONE == "us-central1-f" ]] ; then
     export STORAGE_BUCKET=gs://us_bucketbucket
 elif [[ $VM_ZONE == "europe-west4-a" ]] ; then
@@ -22,6 +31,13 @@ else
     echo
 fi
 
+
+
+
+
+
+# set the group of dataset splits and where to 
+# find them, depending on the task
 if [[ $1 == "Q12" ]] ; then
     export DATA_DIR=${STORAGE_BUCKET}/t2t-data-emheam
     export LIST=(\
@@ -69,43 +85,46 @@ else
     echo DATA_DIR not defined
     echo
 fi
-if [[ $# -eq 4 ]] ; then
-    # Assumed the VM has a tpu already configured
-    export TPU_IP_ADDRESS=$TPU_IP
-    export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
-    export USE_TPU=True
-    export CLOUD_TPU_NAME=$TPU_NAME # jonmcwong-tpu
-    export PROBLEM=algorithmic_math_deepmind_all
-    export MODEL=$2 # transformer
-    export MODEL_TAG=$3 # mds_paper_settings-2020-06-12
-    if [[ $MODEL == "transformer" ]] ; then
-        export HPARAMS_SET=transformer_tpu
-    elif [[ $MODEL == "universal_transformer" ]] ; then
-        # check if global is in MODEL_TAG
-        if [[ $(echo $MODEL_TAG | grep "global") == "" ]] ; then
-            export HPARAMS_SET=adaptive_universal_transformer_base_tpu
-        else
-            export HPARAMS_SET=adaptive_universal_transformer_global_base_tpu
-        fi
+
+
+
+
+# set the flags used in t2t_eval
+export USE_TPU=True
+export CLOUD_TPU_NAME=$TPU_NAME # jonmcwong-tpu
+export PROBLEM=algorithmic_math_deepmind_all
+export MODEL=$2 # transformer
+export MODEL_TAG=$3 # mds_paper_settings-2020-06-12
+if [[ $MODEL == "transformer" ]] ; then
+    export HPARAMS_SET=transformer_tpu
+elif [[ $MODEL == "universal_transformer" ]] ; then
+    # check if global is in MODEL_TAG
+    if [[ $(echo $MODEL_TAG | grep "global") == "" ]] ; then
+        export HPARAMS_SET=adaptive_universal_transformer_base_tpu
     else
-        export HPARAMS_SET"???????"
-        echo
-        echo
-        echo "not sure what HPARAMS_SET to assign for this MODEL"
-        echo
-        echo
+        export HPARAMS_SET=adaptive_universal_transformer_global_base_tpu
     fi
-    export TRAIN_DIR=${STORAGE_BUCKET}/t2t_train/$PROBLEM/$MODEL-$MODEL_TAG
-    export RESULTS_DIR=${STORAGE_BUCKET}/results-$MODEL-$MODEL_TAG
-    export EVAL_USE_TEST_SET=True
-    # echo "mkdir eval-results-"$MODEL_TAG
-    # mkdir "eval-results-"$MODEL_TAG
+else
+    export HPARAMS_SET"???????"
+    echo
+    echo
+    echo "not sure what HPARAMS_SET to assign for this MODEL"
+    echo
+    echo
+fi
+export TRAIN_DIR=${STORAGE_BUCKET}/t2t_train/$PROBLEM/$MODEL-$MODEL_TAG
+export RESULTS_DIR=${STORAGE_BUCKET}/results-$MODEL-$MODEL_TAG
+export EVAL_USE_TEST_SET=True
+export SPLIT_NUM=$4
+export DATASET_SPLIT=${LIST[$SPLIT_NUM]}
+export TASK_DIRECTION=$1
 
-    export SPLIT_NUM=$4
-    export DATASET_SPLIT=${LIST[$SPLIT_NUM]}
 
-    export TASK_DIRECTION=$1
 
+
+
+# run or dry-run
+if [[ $# -eq 4 ]] ; then
     echo
     echo "Running..."
     echo "TPU_IP_ADDRESS = "$TPU_IP_ADDRESS
@@ -157,42 +176,6 @@ if [[ $# -eq 4 ]] ; then
     echo
 
 elif [[ $# -eq 5 && $5 == "--dry-run" ]]; then
-
-    # Assumed the VM has a tpu already configured
-    export TPU_IP_ADDRESS=$TPU_IP
-    export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
-    export USE_TPU=True
-    export CLOUD_TPU_NAME=$TPU_NAME # jonmcwong-tpu
-    export PROBLEM=algorithmic_math_deepmind_all
-    export MODEL=$2 # transformer
-    export MODEL_TAG=$3 # mds_paper_settings-2020-06-12
-    if [[ $MODEL == "transformer" ]] ; then
-        export HPARAMS_SET=transformer_tpu
-    elif [[ $MODEL == "universal_transformer" ]] ; then
-        # check if global is in MODEL_TAG
-        if [[ $(echo $MODEL_TAG | grep "global") == "" ]] ; then
-            export HPARAMS_SET=adaptive_universal_transformer_base_tpu
-        else
-            export HPARAMS_SET=adaptive_universal_transformer_global_base_tpu
-        fi
-    else
-        export HPARAMS_SET"???????"
-        echo
-        echo
-        echo "not sure what HPARAMS_SET to assign for this MODEL"
-        echo
-        echo
-    fi
-
-    export TRAIN_DIR=${STORAGE_BUCKET}/t2t_train/$PROBLEM/$MODEL-$MODEL_TAG
-    export RESULTS_DIR=${STORAGE_BUCKET}/results-$MODEL-$MODEL_TAG
-    export EVAL_USE_TEST_SET=True
-    # echo "mkdir eval-results-"$MODEL_TAG
-    # mkdir "eval-results-"$MODEL_TAG
-
-    export SPLIT_NUM=$4
-    export DATASET_SPLIT=${LIST[$SPLIT_NUM]}
-    export TASK_DIRECTION=$1
 
     echo "Process will run the following:"
     echo "TPU_IP_ADDRESS = "$TPU_IP_ADDRESS
